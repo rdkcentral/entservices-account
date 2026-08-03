1f855f2a8ebc655edc8c537ffe76c69dfbf3d8ee
rajaLucy <rajakiranyadavalli@gmail.com>
2026-08-03 16:05:40 +0530

    RDKEMW-22169:Reverted patches

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -41,9 +41,6 @@ git clone --branch feature/RDKEMW-22169 https://github.com/rdkcentral/entservice
 # Build Thunder-Tools
 echo "======================================================================================"
 echo "building thunderTools"
-cd ThunderTools
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/00010-R4.4.6-Add-support-for-project-dir.patch
-cd -
 
 
 cmake -G Ninja -S ThunderTools -B build/ThunderTools \
@@ -59,12 +56,6 @@ cmake --build build/ThunderTools --target install
 # Build Thunder
 echo "======================================================================================"
 echo "building thunder"
-
-cd Thunder
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/error_code_R4_4_6.patch
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/1004-R4.4.6-Add-support-for-project-dir.patch
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/Jsonrpc_dynamic_error_handling_R4.4.6.patch
-cd -
 
 cmake -G Ninja -S Thunder -B build/Thunder \
     -DMESSAGING=ON \
